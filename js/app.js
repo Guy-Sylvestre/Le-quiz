9b1ed098c16bd84d4f311c7a421cf7b94f4bf35b
KRAKOU Danho Guy Sylvestre <guysylvestre876@gmail.com>
2022-04-18 06:25:27 +0000

    Quizz _ Afficher les résultats

--- js/app.js
+++ js/app.js
@@ -1,13 +1,84 @@
-const form = document.querySelector("form-quizz");
+const form = document.querySelector(".form-quizz");
 let tableauResultats = [];
-console.log("okkkkkkkkkkkkkkkkkk");
+const reponses = ['b', 'c', 'c', 'b', 'b'];
+const emojis = ['✔️','✨','👀','😭','👎'];
+const titreResultat = document.querySelector(".resultats h2"); 
+const noteResultat = document.querySelector(".note");
+const aideResultat = document.querySelector(".aide");
+const toutesLesQuestions = document.querySelector(".question-block");
+let veryArray = [];
 
-form.addEventListener('click', (e) => {
+
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     // console.log(document.querySelector('input[name="q1"]').value);
 
-    for (i =1; i < 6; i++){
-        tableauResultats.push(document.querySelector(`input[name="q${i}]"`).value)
-    }
-    console.log(tableauResultats);
+
+   for (i = 1; i < 6; i++){
+    //    Literals ``
+    tableauResultats.push(document.querySelector(`input[name="q${i}"]:checked`).value);
+   }
+
+   verifyFunction(tableauResultats);
+   tableauResultats = [];
 })
+
+
+function verifyFunction(tabResultats){
+
+    for (let a = 0; a < 5; a++){
+        
+
+        if (tableauResultats[a] === reponses[a]){
+            veryArray.push(true);
+        }else{
+            veryArray.push(false);
+        }
+    }
+
+    afficherResultats(veryArray);
+    veryArray = [];
+}
+
+function afficherResultats(tabCheck){
+    const nbDeFautes = tabCheck.filter(el => el !== true).length;
+    console.log(nbDeFautes);
+
+    switch(nbDeFautes){
+
+        case 0:
+            titreResultat.innerText = `✔️ Bravo, c'est un sans faute ! ✔️`
+            aideResultat.innerText = ''
+            noteResultat.innerText = '5/5'
+            break;
+        case 1:
+            titreResultat.innerText = `✨ Vous y êtes presque ! ✨`
+            aideResultat.innerText = 'Retentez une autre réponse dans la case rouge, puis re-validez !'
+            noteResultat.innerText = '4/5'
+            break;
+        case 2:
+            titreResultat.innerText = `✨ Encore un effort ... 👀`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '3/5'
+            break;
+        case 3:
+            titreResultat.innerText = `👀 Il reste quelques erreurs. 😭`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '2/5'
+            break;
+        case 4:
+            titreResultat.innerText = `😭 Peux mieux faire ! 😭`
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '1/5'
+            break;
+        case 5:
+            titreResultat.innerText = '👎 Tu peux mieux faire ! 👎'
+            aideResultat.innerText = 'Retentez une autre réponse dans les cases rouges, puis re-validez !'
+            noteResultat.innerText = '0/5'
+        break;
+
+        default:
+            'Wops, cas innatendu.';
+
+    }
+}
